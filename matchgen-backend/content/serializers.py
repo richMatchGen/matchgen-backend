2a58fd23ec9152836bf11f50e183041ecc342484
richMatchGen <rich@matchgen.co.uk>
2025-04-01 08:31:55 +0100

    dateFormat

--- matchgen-backend/content/serializers.py
+++ matchgen-backend/content/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from .models import Match
 
 class MatchSerializer(serializers.ModelSerializer):
+    formatted_date = serializers.SerializerMethodField()
     class Meta:
         model = Match
         fields = [
             "match_type", "opponent", "club_logo", "opponent_logo", "sponsor",
-            "date", "time_start", "venue", "location"
+            "date","formatted_date" ,"time_start", "venue", "location"
         ]
+
+    def get_formatted_date(self, obj):
+        return obj.date.strftime("%d/%m/%Y")

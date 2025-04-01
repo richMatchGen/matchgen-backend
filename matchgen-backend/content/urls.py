e0f3767bb27a3e2b90a6a8ff26d7c86c57667307
richMatchGen <rich@matchgen.co.uk>
2025-04-01 09:14:20 +0100

    PlayerListCreateViewURL

--- matchgen-backend/content/urls.py
+++ matchgen-backend/content/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MatchListCreateView
+from .views import MatchListCreateView,PlayerListCreateView
 
 urlpatterns = [
     path("matches/", MatchListCreateView.as_view(), name="match-list-create"),
+    path("players/", PlayerListCreateView.as_view(), name="player-list-create"),
 ]

e8b9871a02372011733d5d2c8cb0c4b96c0804d0
cal.mccullagh <cal.mccullagh@nanoporetech.com>
2025-06-03 18:53:24 +0100

    Skip security check on django secret key

--- matchgen-backend/matchgen/settings.py
+++ matchgen-backend/matchgen/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-14k-bd&@uce*9^)(3ysp9ao$(10)-0ckur9%_k@4et&$n0y=j="
+SECRET_KEY = (
+    "django-insecure-14k-bd&@uce*9^)(3ysp9ao$(10)-0ckur9%_k@4et&$n0y=j="  # nosec B105
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 

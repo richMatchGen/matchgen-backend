96b3568396f139fe07c28449b4d219b376653082
richMatchGen <rich@matchgen.co.uk>
2025-08-18 12:38:22 +0100

    Fix match creation 500 error: Add club field to serializers with read-only protection

--- matchgen-backend/content/serializers.py
+++ matchgen-backend/content/serializers.py
@@ -9,43 +9,8 @@ class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
         fields = [
-            "match_type",
-            "opponent",
-            "club_logo",
-            "opponent_logo",
-            "sponsor",
-            "date",
-            "formatted_date",
-            "time_start",
-            "venue",
-            "location",
-        ]
-
-    def get_formatted_date(self, obj):
-        return obj.date.strftime("%d/%m/%Y")
-
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        fields = [
             "id",
-            "name",
-            "squad_no",
-            "player_pic",
-            "formatted_pic",
-            "position",
-            "sponsor",
-        ]
-
-
-class FixturesSerializer(serializers.ModelSerializer):
-    formatted_date = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Match
-        fields = [
-            "id",
+            "club",
             "match_type",
             "opponent",
             "club_logo",
@@ -58,6 +23,49 @@ class FixturesSerializer(serializers.ModelSerializer):
             "location",
             "matchday_post_url",
         ]
+        read_only_fields = ["id", "club"]  # club is set by the view, not the client
+
+    def get_formatted_date(self, obj):
+        return obj.date.strftime("%d/%m/%Y")
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = [
+            "id",
+            "club",
+            "name",
+            "squad_no",
+            "player_pic",
+            "formatted_pic",
+            "position",
+            "sponsor",
+        ]
+        read_only_fields = ["id", "club"]  # club is set by the view, not the client
+
+
+class FixturesSerializer(serializers.ModelSerializer):
+    formatted_date = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Match
+        fields = [
+            "id",
+            "club",
+            "match_type",
+            "opponent",
+            "club_logo",
+            "opponent_logo",
+            "sponsor",
+            "date",
+            "formatted_date",
+            "time_start",
+            "venue",
+            "location",
+            "matchday_post_url",
+        ]
+        read_only_fields = ["id", "club"]
 
     def get_formatted_date(self, obj):
         return obj.date.strftime("%d/%m/%Y")
